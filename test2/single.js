ee9f300c0726ab2f24467927a5b586b4c869fb17
Michael Pantaleon <mkpantaleon@hotmail.com>
2020-11-17 12:51:01 -0500

    first commit

---
+++ test2/single.js
@@ -0,0 +1,102 @@
+
+Survey
+    .StylesManager
+    .applyTheme("default");
+
+var json = {
+    title: "Employee Reference Check",
+    pages: [{
+        questions: [
+            {
+                //Question 1
+                name: "Question1",
+                type: "text",
+                title: "What is your relationship with the candidate?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 2
+                name: "Question2",
+                type: "text",
+                title: "How would you describe the candidate’s management style?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 3
+                name: "Question3",
+                type: "text",
+                title: "Did he / she effectively communicate their responsibilities in previous roles?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 4
+                name: "Question4",
+                type: "text",
+                title: "How did the candidate handle stress/conflict/pressure?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 5
+                name: "Question5",
+                type: "text",
+                title: "How would you describe the candidate’s communication/listening skills?",
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 6
+                name: "Question6",
+                type: "text",
+                title: "What qualities about the candidate make him/her a great leader? Are there any qualities that don't?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 7
+                name: "Question7",
+                type: "text",
+                title: "What are the candidate's biggest strengths and areas for improvement?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 8
+                name: "Question8",
+                type: "text",
+                title: "Did the candidate ever supervise other employees? How effectively?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 9
+                name: "Question9",
+                type: "text",
+                title: "What type of environment do you think the candidate would thrive in (describe culture, team, management, autonomy)?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+            {
+                //Question 10
+                name: "Question10",
+                type: "text",
+                title: "If you were the hiring manager, would you hire the candidate?",                   
+                placeHolder: "",
+                isRequired: true
+            },
+        ]
+    }],
+};
+
+window.survey = new Survey.Model(json);
+
+survey
+    .onComplete
+    .add(function (result) {
+        document.location = "singlethanks.html";
+    });
+
+$("#surveyElement").Survey({ model: survey });
